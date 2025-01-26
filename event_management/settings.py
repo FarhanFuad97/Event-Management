538bfabb8302377f9d776b74b80d9bf1bdc96ab8
Farhan Fuad <farhanfuad783@gmail.com>
2025-01-26 20:18:33 +0600

    addad requirments.txt file

--- event_management/settings.py
+++ event_management/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-e4lb_571*hmh!i-)c5j(-=_nf^dv$e+_i0gktqj$#9#hfmq5(1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = ['https://*.onrender.com', 'http://127.0.0.1:8000']
 
 
 # Application definition
